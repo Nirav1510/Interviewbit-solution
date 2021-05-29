20c3de24f29ae1b21e9a45b389ab33beeab94520
Nirav <83664237+Nirav1510@users.noreply.github.com>
2021-05-29 17:12:01 +0530

    Update Rotated_sorted_array_search.cpp

--- Binary Search/Rotated_sorted_array_search.cpp
+++ Binary Search/Rotated_sorted_array_search.cpp
@@ -40,13 +40,54 @@ int search(const vector<int> &A, int B)
     }
     return -1;
 }
+//method 2
+int findPivot(const vector<int> &A)
+{
+    int l = 0, h = A.size() - 1;
+    while (l <= h)
+    {
+        int mid = h - (h - l) / 2;
+        if (A[mid] > A[0])
+            l = mid + 1;
+        else
+            h = mid - 1;
+    }
+    return h;
+}
+int isPresent(const vector<int> &A, int l, int h, int val)
+{
+
+    while (l <= h)
+    {
+        int mid = h - (h - l) / 2;
+        if (A[mid] == val)
+            return mid;
+        else if (A[mid] < val)
+            l = mid + 1;
+        else
+            h = mid - 1;
+    }
+    return -1;
+}
+int search1(const vector<int> &A, int B)
+{
+    int pivot = findPivot(A);
+    if (B == A[pivot])
+        return pivot;
+    else if (B < A[0])
+        return isPresent(A, pivot + 1, A.size() - 1, B);
+    else
+        return isPresent(A, 0, pivot, B);
+}
 int main()
 {
     vector<int> A = {10, 20, 50, 40, 60, 90, 30};
     int b;
     cin >> b;
     int res = search(A, b);
+    int res1 = search1(A, b);
     cout << res << endl;
+    cout << res1 << endl;
 
     return 0;
 }
