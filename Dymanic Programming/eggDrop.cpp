8bd12b6917a267b93a143396e43b8c5fe12ea7fb
Nirav <83664237+Nirav1510@users.noreply.github.com>
2021-06-14 10:16:44 +0530

    Update eggDrop.cpp

--- Dymanic Programming/eggDrop.cpp
+++ Dymanic Programming/eggDrop.cpp
@@ -35,6 +35,49 @@ int superEggDropHelp(int e, int f)
     return dp[e][f];
 }
 
+//2nd 
+
+int dp[101][10001];
+int esolve(int e, int f){
+    if(f==1 || f==0 || e==1) return f;
+    
+    if(dp[e][f]!=-1){
+        return dp[e][f];
+    }
+    
+    int ans=INT_MAX;
+    int l=1,h=f;
+    
+    while(l<=h){
+        int mid=l+(h-l)/2;
+        int left,right;
+        
+        if(dp[e-1][mid-1]!=-1){
+            left=dp[e-1][mid-1];
+        }
+        else{
+            left=esolve(e-1,mid-1);
+            dp[e-1][mid-1]=left;
+        }
+        if(dp[e][f-mid]!=-1){
+            right=dp[e][f-mid];
+        }
+        else{
+            right=esolve(e,f-mid);
+            dp[e][f-mid]=right;
+        }
+        int temp=1+max(left,right);
+        if(left<right){
+            l=mid+1;
+        }
+        else{
+            h=mid-1;
+        }
+        ans=min(ans,temp);
+    }
+    return dp[e][f]=ans;
+}
+
 int Solution::solve(int e, int f)
 {
     memset(dp, -1, sizeof(dp));
