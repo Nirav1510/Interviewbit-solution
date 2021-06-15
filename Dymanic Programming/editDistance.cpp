17446208d4bb42c25938c5a9d1fbc4ca8dea8e98
Nirav <83664237+Nirav1510@users.noreply.github.com>
2021-06-15 23:31:40 +0530

    Update editDistance.cpp

--- Dymanic Programming/editDistance.cpp
+++ Dymanic Programming/editDistance.cpp
@@ -1,3 +1,52 @@
+int dp[101][10001];
+
+int solve(int e, int f){
+    if(f==1 || f==0 || e==1) return f;
+    
+    if(dp[e][f]!=-1){
+        return dp[e][f];
+    }
+    
+    int ans=INT_MAX;
+    int l=1,h=f;
+    
+    while(l<=h){
+        int mid=l+(h-l)/2;
+        int left,right;
+        
+        if(dp[e-1][mid-1]!=-1){
+            left=dp[e-1][mid-1];
+        }
+        else{
+            left=solve(e-1,mid-1);
+            dp[e-1][mid-1]=left;
+        }
+        if(dp[e][f-mid]!=-1){
+            right=dp[e][f-mid];
+        }
+        else{
+            right=solve(e,f-mid);
+            dp[e][f-mid]=right;
+        }
+        int temp=1+max(left,right);
+        if(left<right){
+            l=mid+1;
+        }
+        else{
+            h=mid-1;
+        }
+        ans=min(ans,temp);
+    }
+    return dp[e][f]=ans;
+}
+
+int Solution::Solve(int e, int f) {
+    memset(dp,-1,sizeof(dp));
+    int res=solve(e,f);
+    
+    return res;
+}
+
 int Solution::minDistance(string A, string B)
 {
     int n = A.length(), m = B.length();
