2f73e9f33946ace2ff7d15a4395ab5df731ef63d
Nirav Gusai <83664237+Nirav1510@users.noreply.github.com>
2021-09-15 22:33:05 +0530

    Create sortByColor.cpp

---
+++ Two pointers/sortByColor.cpp
@@ -0,0 +1,8 @@
+void Solution::sortColors(vector<int> &A) {
+    int i=0, j=0, k=A.size()-1; 
+    while(j<=k) {
+        if(A[j] == 0)   swap(A[i++], A[j]);
+        if(A[j] == 2)   swap(A[j], A[k--]);
+        else j++;
+    }
+}
